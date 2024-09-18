f8b318f29be84f502c6bfabca31d78969b24f756
Marco <mt@mt88.eu>
2024-09-18 21:14:02 +0200

    apply daylight savings tim to logs

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,6 +1,7 @@
 #include "Logging.h"
 #include "Arduino.h"
 #include <WiFiClient.h>
+#include "ntpclient.h"
 const int LOG_DATA_SIZE = 40;		//> 40 makes webinterface unstable after some time'
 String LOG_DATA[LOG_DATA_SIZE];
 int LOG_DATA_INDEX = 0;
@@ -11,6 +12,17 @@ void Log(String message) {
 
 void Log(String message, bool dummyTime) {
 	Serial.println(message.c_str());
+	
+	int int_hour = hour();
+	if (summertime_EU(year(), month(), day(), hour(), 1)) {
+		if (int_hour == 24) {
+			int_hour = 0;
+		}
+		else {
+			int_hour = int_hour + 1;
+		}
+	}
+
 	if (LOG_DATA_INDEX > LOG_DATA_SIZE - 1) {
 		for (int i = 1; i < LOG_DATA_SIZE; i++) {
 			LOG_DATA[i - 1] = LOG_DATA[i];
@@ -23,7 +35,7 @@ void Log(String message, bool dummyTime) {
 		LOG_DATA[LOG_DATA_INDEX] = logString;
 	}
 	else {
-		sprintf(logString, "%02d.%02d. %02d:%02d:%02d - %s", day(), month(), hour(), minute(), second(), message.c_str());
+		sprintf(logString, "%02d.%02d. %02d:%02d:%02d - %s", day(), month(), int_hour, minute(), second(), message.c_str());
 		LOG_DATA[LOG_DATA_INDEX] = logString;
 	}
 	LOG_DATA_INDEX++;
